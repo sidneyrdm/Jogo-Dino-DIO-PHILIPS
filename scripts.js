3943627d87909ebecea6ee441defbab3d563d287
sidneyrdm <sidneybritomal@gmail.com>
2022-05-26 20:48:36 -0300

    iniciando a captura das teclas pressionadas

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
 const dino = document.querySelector('.dino');
+
+function handleKeyUp(event){
+  if(event.keyCode === 32){
+      console.log('pressionou espaço');
+  }
+}
+
+document.addEventListener('keyup', handleKeyUp);
