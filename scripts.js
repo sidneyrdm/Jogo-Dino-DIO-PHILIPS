e9bb3408267fa61f4a7ac3d114e939c2521fafe6
sidneyrdm <sidneybritomal@gmail.com>
2022-05-26 21:19:15 -0300

    iniciando a criação do elemento cactus

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        console.log('pressionou espaço');
-        jump();
+        if(!isJumping)    
+        jump();    
     }
 }
 
 function jump() {
     let position = 0;
+
+    isJumping = true;
+
     let upInterval = setInterval(() => {
         if (position >= 150) {
             clearInterval(upInterval);
@@ -16,18 +21,32 @@ function jump() {
             let downInterval = setInterval(() => {
                 if (position <= 0) {
                     clearInterval(downInterval);
+
+                    isJumping = false;
+
                 } else {
                     position -= 20;
                     dino.style.bottom = position + 'px';
                 }
-            }, 20);
+            }, 30);
         } else {
             position += 20;
 
             dino.style.bottom = position + 'px';
         }
 
-    }, 20);
+    }, 30);
 }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    cactus.classList.add('cactus');
+
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
