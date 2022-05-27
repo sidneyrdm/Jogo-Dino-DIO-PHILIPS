455028a7b5fd01e55188971705b3358517fde52f
sidneyrdm <sidneybritomal@gmail.com>
2022-05-26 21:02:15 -0300

    inicio de efeito de subida e descida do dino

--- scripts.js
+++ scripts.js
@@ -1,9 +1,33 @@
 const dino = document.querySelector('.dino');
 
-function handleKeyUp(event){
-  if(event.keyCode === 32){
-      console.log('pressionou espaço');
-  }
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        console.log('pressionou espaço');
+        jump();
+    }
+}
+
+function jump() {
+    let position = 0;
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        } else {
+            position += 20;
+
+            dino.style.bottom = position + 'px';
+        }
+
+    }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
